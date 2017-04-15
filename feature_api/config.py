61b70eb20b1303fd221c2d09ac22a5d11f6a1723
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-04-15 12:48:16 +0300

    Add configuration setup

--- feature_api/config.py
+++ feature_api/config.py
@@ -1,1 +1,12 @@
 """Config validation setup."""
+import trafaret as t
+
+
+CONFIG_TRAFARET = t.Dict({
+    t.Key('DEBUG'): t.Bool,
+    t.Key('DATABASE_NAME'): t.String,
+    t.Key('DATABASE_USERNAME'): t.String,
+    t.Key('DATABASE_PASSWORD'): t.String,
+    t.Key('DATABASE_HOST'): t.String,
+    t.Key('DATABASE_PORT'): t.Int
+})
