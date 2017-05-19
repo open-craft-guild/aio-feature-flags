811263573aa35361da8a8ddde03b333914e156c5
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-05-19 18:30:21 +0300

    Add default status=200 to async_json_out decorator

--- web_utils.py
+++ web_utils.py
@@ -6,13 +6,13 @@ from inspect import iscoroutine
 from aiohttp.web import json_response
 
 
-def async_json_out(orig_method=None, *, content_type='application/json', **kwargs):
+def async_json_out(orig_method=None, *, status=200, content_type='application/json', **kwargs):
     """Turn dict output of an HTTP handler into JSON response.
 
     Decorates aiohttp request handlers.
     """
     if orig_method is None:
-        return partial(async_json_out, content_type='application/json', **kwargs)
+        return partial(async_json_out, status=200, content_type='application/json', **kwargs)
 
     @wraps(orig_method)
     async def wrapper(*args, **kwargs):
@@ -23,6 +23,7 @@ def async_json_out(orig_method=None, *, content_type='application/json', **kwarg
 
         return json_response(
             dict_resp,
+            status=status,
             content_type=content_type,
             **kwargs
         )
