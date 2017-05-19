a8630ca7f16c93ac5a14d86a95fd2c565ac3b2df
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-05-19 18:31:55 +0300

    Unshadow kwargs in async_json_out decorator

--- web_utils.py
+++ web_utils.py
@@ -6,13 +6,13 @@ from inspect import iscoroutine
 from aiohttp.web import json_response
 
 
-def async_json_out(orig_method=None, *, status=200, content_type='application/json', **kwargs):
+def async_json_out(orig_method=None, *, status=200, content_type='application/json', **dec_kwargs):
     """Turn dict output of an HTTP handler into JSON response.
 
     Decorates aiohttp request handlers.
     """
     if orig_method is None:
-        return partial(async_json_out, status=200, content_type='application/json', **kwargs)
+        return partial(async_json_out, status=200, content_type='application/json', **dec_kwargs)
 
     @wraps(orig_method)
     async def wrapper(*args, **kwargs):
@@ -25,6 +25,6 @@ def async_json_out(orig_method=None, *, status=200, content_type='application/js
             dict_resp,
             status=status,
             content_type=content_type,
-            **kwargs
+            **dec_kwargs
         )
     return wrapper
